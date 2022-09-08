8473585dcaef5ca992c5bf7dfc30cdd35ead816d
agusserraa98 <agustin.sebastian.serra@gmail.com>
2022-09-08 13:53:06 -0300

    POST a firebase y parseo de informacion al traerla

--- src/components/my_components/FormGeneral.js
+++ src/components/my_components/FormGeneral.js
@@ -1,24 +1,62 @@
-import { Alert, Button, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
-import { get, now } from "lodash";
+import { Alert, Button, Card, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import { get, now, set } from "lodash";
 import { useForm } from "react-hook-form";
 import Swal from 'sweetalert2'
+import { useEffect, useState } from "react";
+import { async } from "@firebase/util";
 
-import format from "date-fns/format";
+// firebase
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoc } from 'firebase/firestore';
+import firebaseApp from '../../firebase/firebase';
 
 const FormGeneral = () => {
+  // variables
+  const db = getFirestore(firebaseApp);
+  const [lista, setLista] = useState([])
+  // funciones
   const { register, handleSubmit, formState: { errors } } = useForm({
     defaultValues: {
       origenVenta: "reparto",
       monto: null
     }
   });
-
-  const onSubmit = (data) => {
+  // submit
+  const onSubmit = (data,items) => {
     const horax = +new Date();
     console.log(horax)
-    data.hora =  horax;
+    data.hora = horax;
     console.log(data)
+    getPedidos()
+    try {
+      addDoc(collection(db, 'ingresos'), {
+        data
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
+  
+  async function getPedidos() {
+    try {
+      const items = [];
+      const response = await getDocs(collection(db, 'ingresos'))
+      response.forEach((item)=>{
+        items.push({...item.data(), id:item.id});
+      })
+      console.log(items)
+      items.map((item)=>console.log(item.data.monto))
+      setLista(items)
+      return items;
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+useEffect(()=>{
+  console.log("me modifique")
+},[lista])
+
+
 
   const handleError = (errors) => console.log(errors);
   return (
@@ -26,7 +64,7 @@ const FormGeneral = () => {
       <form onSubmit={handleSubmit(onSubmit, handleError)}>
         <Stack spacing={2}>
           <h2>Ingreos de datos</h2>
-          <Select 
+          <Select
             {...register("origenVenta", {
               required: {
                 value: true,
@@ -34,9 +72,9 @@ const FormGeneral = () => {
               },
               maxlenght: 10
             })}>
-          <MenuItem value={"negocio"}>Negocio</MenuItem>
-          <MenuItem value={"reparto"}>Reparto</MenuItem>
-          <MenuItem value={"individual"}>Venta individual</MenuItem>
+            <MenuItem value={"negocio"}>Negocio</MenuItem>
+            <MenuItem value={"reparto"}>Reparto</MenuItem>
+            <MenuItem value={"individual"}>Venta individual</MenuItem>
           </Select>
           <Typography variant="span" color="red" >
             {errors?.origenVenta?.message}
@@ -59,6 +97,19 @@ const FormGeneral = () => {
           <Button type='submit'>Submit</Button>
         </Stack>
       </form>
+
+      <div>
+        <Stack spacing={2}>  
+        { lista.map(item=>
+            <Card key = {item.id}>
+              <Typography variant="p">{item.data.origenVenta}</Typography>
+              <Typography variant="h5">{item.data.monto}</Typography>
+              <Typography variant="h5">{item.data.hora}</Typography>
+            </Card>
+        )
+        }
+        </Stack>
+      </div>
     </div>
   );
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,6 @@
 // material
 import { Container, Stack, Typography } from '@mui/material';
 
-// firebase
-import {getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoc} from 'firebase/firestore';
-import firebaseApp from '../firebase/firebase';
-
 
 // componentes
 import Page from '../components/Page';
@@ -20,12 +16,14 @@ import PRODUCTS from '../_mock/products';
 // ----------------------------------------------------------------------
 
 export default function EcommerceShop() {
-  const db = getFirestore(firebaseApp);
+
+
+
+
   return (
     <Page title="Probando giladas">
       <Container>
         <FormGeneral />
-        <Typography variant='h3'> Aca va a ir una tabla con los ingresos del usuario</Typography>
       </Container>
     </Page>
   );
